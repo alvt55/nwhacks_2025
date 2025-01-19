d9bbf54282b0453bde497fd67d3c7571c4354de9
Owen-Wangg <owenwang26@gmail.com>
2025-01-18 20:17:13 -0800

    encrypt and decrypt function

--- cli/cli.py
+++ cli/cli.py
@@ -1,6 +1,19 @@
+from cryptography.fernet import Fernet
+
 def main():
     print("Hello World!")
 
 
 if __name__ == "__main__":
     main()
+
+def encrypt(value: str):
+    key = Fernet.generate_key()
+    cipher_suite = Fernet(key)
+    encoded_key = cipher_suite.encrypt(value.encode())
+    return key, encoded_key
+    
+def decrypt(key: bytes, encoded_key: bytes):
+    cipher_suite = Fernet(key)
+    decoded_key = cipher_suite.decrypt(encoded_key)
+    return decoded_key.decode()
